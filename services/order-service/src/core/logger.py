c0ebf2c6fa4d5718aa7a4a59d5be8dc03591078e
Gabriela Mota <gabrielamota.unicamp@gmail.com>
2025-06-18 10:55:40 -0300

    Refactor logger helpers

--- services/order-service/src/core/logger.py
+++ services/order-service/src/core/logger.py
@@ -1,6 +1,7 @@
 import logging
 
 
-def get_logger(name="user-service"):
+def get_logger(name: str = __name__):
+    """Return a configured logger using the provided name."""
     logging.basicConfig(level=logging.INFO)
     return logging.getLogger(name)
